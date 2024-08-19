1b0ede4a74cb8ba568fc4abb2bd4de41fffec3de
SAGAR T <ssagu6661@gmail.com>
2024-08-19 11:43:20 -0400

    Add files via upload

---
+++ c15.py
@@ -0,0 +1,18 @@
+
+# If Else
+print(               "Enter  Any Two Numbers, \nTo Check greater, Less and Equal")
+a=float(input("a>>   "))
+b=float(input("b>>   "))
+print("==========ANSWER=============")
+if a>b:
+    print("A is greater than b")
+elif a<b: 
+    print("A is less than B")
+elif a==b:
+    print("A is equal to b")
+
+else :
+    print("SORRY")
+    
+    
+    
